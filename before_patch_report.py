ea9a8a590bdf25e77e8f50a618138640770cb808
lemiliomoreno <lemiliomoreno@gmail.com>
2018-11-13 00:47:19 -0600

    Update before_patch_report.py

--- before_patch_report.py
+++ before_patch_report.py
@@ -1,10 +1,35 @@
 import subprocess as sp
+import re
+import time
+
+BETWEEN_METHODS="--------------------"
+
+def checking_server_release():
+    print("Checking the server release...")
+    SEPARATOR_LOCATION=13
+    first_for_release_check = ['/bin/cat', '/etc/os-release']
+    second_for_release_check = ['/bin/grep', 'PRETTY_NAME']
+    
+    redhat_string = "redhat"
+    centos_string = "centos"
+    ubuntu_string = "ubuntu"
+
+    p1 = sp.Popen(first_for_release_check, stdout=sp.PIPE)
+    p2 = sp.Popen(second_for_release_check, stdin=p1.stdout, stdout=sp.PIPE)
+
+    p1.stdout.close()
+
+    release = p2.communicate()[0].decode('utf-8')
+
+    if(re.search(redhat_string, release, re.I) or re.search(centos_string, release, re.I)): print("OK_TO_PATCH: Release is in scope: {0}.".format(release[SEPARATOR_LOCATION:-2]))
+    else: print("CANT_PATCH: Release isn't in scope: {0}.".format(release[SEPARATOR_LOCATION:-2]))
+
+    p2.stdout.close()
 
 def checking_for_root_space():
-    print("********************")
-    print("Checking if root has more than 20% space available...")
-    first_for_root_check = ['df', '/']
-    second_for_root_check = ['awk', """NR==2 {print $5}"""]
+    print("Checking if /root has more than 20% space available...")
+    first_for_root_check = ['/bin/df', '/']
+    second_for_root_check = ['/usr/bin/awk', 'NR==2 {print $5}']
 
     p1 = sp.Popen(first_for_root_check, stdout=sp.PIPE)
     # stdin of p2, will be the p1.stdout
@@ -15,13 +40,31 @@ def checking_for_root_space():
 
     # Access to communicate()[0] because is stdout, comminucate()[1] is stdeer, it returns a tuple
     # communicate() returns (stdout, stderr)
-    out_to_test = p2.communicate()[0].decode('utf-8')
+    root_space = p2.communicate()[0].decode('utf-8')
     
-    if(out_to_test[:3] == '100'): print("CANT_PATCH: Root (/) has {0}% space left, need more than 20%.".format(int(out_to_test[:3]) - 100))
-    elif(int(out_to_test[:2]) >= 80): print("CANT_PATCH: Root (/) has {0}% space left, need more than 20%.".format(abs(int(out_to_test[:2]) - 100)))
-    else: print("OK_TO_PATCH: Root (/) has {0}% space left.".format(abs(int(out_to_test[:2]) - 100)))
+    if(root_space[:3] == '100'): print("CANT_PATCH: Root (/) has {0}% space left, need more than 20%.".format(int(root_space[:3]) - 100))
+    elif(int(root_space[:2]) >= 80): print("CANT_PATCH: Root (/) has {0}% space left, need more than 20%.".format(abs(int(root_space[:2]) - 100)))
+    else: print("OK_TO_PATCH: Root (/) has {0}% space left.".format(abs(int(root_space[:2]) - 100)))
 
     p2.stdout.close()
-    print("********************")
 
-checking_for_root_space()
+def start_script():
+    print(BETWEEN_METHODS)
+    start_time = time.time()
+    print("STARTED AT: {0}.".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(start_time))))
+
+    print(BETWEEN_METHODS)
+    checking_server_release()
+
+    print(BETWEEN_METHODS)
+    checking_for_root_space()
+
+    print(BETWEEN_METHODS)
+    finish_time = time.time()
+    print("FINISHED AT: {0}.".format(time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(finish_time))))
+    finish_time -= start_time
+    print("It took {0:.4f} seconds to run.".format(finish_time))
+    print(BETWEEN_METHODS)
+
+start_script()
+
