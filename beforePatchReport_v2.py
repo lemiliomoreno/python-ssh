7268bcb1b12d7632ed53885aa66f7740055226e4
lemiliomoreno <lemiliomoreno@gmail.com>
2018-11-21 12:11:34 -0600

    Update beforePatchReport_v2.py

--- beforePatchReport_v2.py
+++ beforePatchReport_v2.py
@@ -4,11 +4,11 @@ import time
 
 BETWEEN_METHODS='--------------------'
 
-commands = {'release' : [['/bin/cat', '/etc/os-release'], ['/bin/grep', 'PRETTY_NAME']],
-            'qpk' : [['/usr/bin/rpm', '-qa'], ['sort', '-r']],
-            'root_space' : [['/bin/df', '/'], ['/usr/bin/awk', 'NR==2 {print $5}']],
-            'kernel' : [['/usr/bin/rpm', '-qa'], ['/usr/bin/sort', '-r']]
-            }
+commands = {'release' : ['/bin/cat', '/etc/redhat-release'],
+                'qpk' : [['/usr/bin/rpm', '-qa'], ['sort', '-r']],
+                'root_space' : [['/bin/df', '/'], ['/usr/bin/awk', 'NR==2 {print $5}']],
+                'kernel' : [['/usr/bin/rpm', '-qa'], ['/usr/bin/sort', '-r']]
+                }
 
 regular_expressions = {'kernel' : ['kernel-[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}-[0-9]{1,3}', 999],
                         'firmware' : ['kernel-tools-[0-9]{1,2}\.[0-9]{1,2}\.[0-9]{1,2}-[0-9]{1,3}', 999],
@@ -25,23 +25,23 @@ class server():
                 self.qpk = None
                 self.root_space = None
                 self.kernel = {'kernel' : None, 'firmware' : None, 'devel' : None}
+                self.start_server_check()
 
         def get_command_output(self, first_command, second_command):
                 print("Sending command to the shell...")
                 print("Command 1: {0}, command 2: {1}".format(first_command, second_command))
+                if(second_command == None):
+                        p1 = sp.Popen(first_command, stdout=sp.PIPE)
+                        return p1.communicate()[0].decode('utf-8')
+                else:
+                        p1 = sp.Popen(first_command, stdout=sp.PIPE)
+                        p2 = sp.Popen(second_command, stdin=p1.stdout, stdout=sp.PIPE)
 
-                p1 = sp.Popen(first_command, stdout=sp.PIPE)
-                p2 = sp.Popen(second_command, stdin=p1.stdout, stdout=sp.PIPE)
+                        p1.stdout.close()
 
-                p1.stdout.close()
-
-                output = p2.communicate()[0].decode('utf-8')
-
-                return output
-
+                        return p2.communicate()[0].decode('utf-8')
         def get_release(self, output):
-                SEPARATOR_LOCATION=13
-                if(re.search(release_strings['redhat'], output, re.I) or re.search(release_strings['centos'], output, re.I)): self.release = output[SEPARATOR_LOCATION:-2]
+                if(re.search(release_strings['redhat'], output, re.I) or re.search(release_strings['centos'], output, re.I)): self.release = output[:-1]
                 else: self.release = False
 
         def get_qpk(self, output):
@@ -64,6 +64,7 @@ class server():
 
         def make_report(self):
                 print(BETWEEN_METHODS)
+
                 print('Server release: {0}'.format(self.release))
                 print('QPK version: {0}'.format(self.qpk))
 
@@ -77,16 +78,16 @@ class server():
                 else: print('Kernel-firmware: {0}, no update neeeded'.format(self.kernel['firmware']))
 
                 if(int(self.kernel['devel'][-3:]) < regular_expressions['devel'][1]): print('Kernel-devel: {0}, needed version: {1}'.format(self.kernel['devel'], regular_expressions['devel'][1]))
-                else: print('Kernel-devel: {0}, no update needed'.format(self.kernel['devel']))
-
+                else: print('Kernel-devel: {0}, no update needed'.format(self.kernel['devel']))  
+                
                 print(BETWEEN_METHODS)
 
         def start_server_check(self):
-                self.get_release(self.get_command_output(commands['release'][0], commands['release'][1]))
+                self.get_release(self.get_command_output(commands['release'], None))
                 self.get_qpk(self.get_command_output(commands['qpk'][0], commands['qpk'][1]))
                 self.get_root_space(self.get_command_output(commands['root_space'][0], commands['root_space'][1]))
                 self.get_kernel(self.get_command_output(commands['kernel'][0], commands['kernel'][1]))
+                self.make_report()
 
-a=server()
-a.start_server_check()
-a.make_report()
+test_server = server()
+                
