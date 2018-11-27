73387ccb34dc63935f5b233779a38c702f3833f4
lemiliomoreno <lemiliomoreno@gmail.com>
2018-11-27 09:45:34 -0600

    Changes
    Added exception for scp and ssh
    Added password parameter by command, needed sshpass

--- what_to_do.sh
+++ what_to_do.sh
@@ -1,15 +1,28 @@
 #!/bin/bash
 
-USERNAME="morenodl"
+USERNAME="YOUR_LINUX_USER"
 SERVERS=$(cat ${HOME}/before_patch_report/servers.txt)
 SCRIPT_PATH="/home/${USERNAME}/before_patch_report/before_patch_check.py"
 COMMAND_PATH="/home/${USERNAME}/before_patch_report/commands.sh"
 REMOTE_PATH="/home/${USERNAME}"
+PASSWORD_PATH="/home/${USERNAME}/before_patch_report/passwd.txt"
 
 for server in ${SERVERS}
 do
         echo "--------------------"
         echo "Connecting to ${server}..."
-        scp ${SCRIPT_PATH} ${USERNAME}@${server}:${REMOTE_PATH}
-        ssh ${USERNAME}@${server} < ${COMMAND_PATH}
+        sshpass -f ${PASSWORD_PATH} scp ${SCRIPT_PATH} ${USERNAME}@${server}:${REMOTE_PATH}
+        if [ $? -ne 0 ]
+        then
+                echo "SCP Error in ${server}"
+        else
+                echo "SCP Done"
+        fi
+        sshpass -f ${PASSWORD_PATH} ssh ${USERNAME}@${server} < ${COMMAND_PATH}
+        if [ $? -ne 0 ]
+        then
+                echo "SSH Error in ${server}"
+        else
+                echo "SSH Done"
+        fi
 done
