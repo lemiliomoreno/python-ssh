03663310be6512e34f95f284b58132ce23d849bd
lemiliomoreno <lemiliomoreno@gmail.com>
2018-11-29 09:31:17 -0600

    Update before_patch_check.py

--- before_patch_check.py
+++ before_patch_check.py
@@ -1,10 +1,11 @@
 import subprocess as sp
 import re
 import time
+import datetime
 
 BETWEEN_METHODS='--------------------'
 
-commands = {'hostname' : 'hostname',
+commands = {'hostname' : ['hostname', '-f'],
                 'release' : ['/bin/cat', '/etc/redhat-release'],
                 'qpk' : [['/bin/rpm', '-qa'], ['/bin/sort', '-r']],
                 'root_space' : [['/bin/df', '/'], ['/usr/bin/awk', 'NR==2 {print $5}']],
@@ -22,6 +23,22 @@ release_strings = {'redhat' : 'red hat', 'centos' : 'centos', 'ubuntu' : 'ubuntu
 
 repos_strings = {'linxcoe' : 'linuxcoe-.*\.repo', 'hpit' : 'hpit-.*\.repo', 'btdt' : 'btdt-.*\.repo', 'epel' : 'epel.repo', 'rhel' : 'rhel-.*\.repo'}
 
+def get_command_output(first_command, second_command):
+        #print("Sending command to the shell...")
+        #print("Command 1: {0}, command 2: {1}".format(first_command, second_command))
+        if(second_command == None):
+                p1 = sp.Popen(first_command, stdout=sp.PIPE)
+
+                return p1.communicate()[0].decode('utf-8')
+
+        else:
+                p1 = sp.Popen(first_command, stdout=sp.PIPE)
+                p2 = sp.Popen(second_command, stdin=p1.stdout, stdout=sp.PIPE)
+
+                p1.stdout.close()
+
+                return p2.communicate()[0].decode('utf-8')
+              
 class server():
 
         def __init__(self):
@@ -31,22 +48,8 @@ class server():
                 self.root_space = None
                 self.kernel = {'kernel' : None, 'firmware' : None, 'devel' : None}
                 self.repos = list()
+                self.time_to_run = time.time()
                 self.start_server_check()
-
-        def get_command_output(self, first_command, second_command):
-                print("Sending command to the shell...")
-                print("Command 1: {0}, command 2: {1}".format(first_command, second_command))
-                if(second_command == None):
-                        p1 = sp.Popen(first_command, stdout=sp.PIPE)
-
-                        return p1.communicate()[0].decode('utf-8')
-                 else:
-                        p1 = sp.Popen(first_command, stdout=sp.PIPE)
-                        p2 = sp.Popen(second_command, stdin=p1.stdout, stdout=sp.PIPE)
-
-                        p1.stdout.close()
-
-                        return p2.communicate()[0].decode('utf-8')
 
         def get_hostname(self, output):
                 self.hostname = output[:-1]
@@ -81,56 +84,62 @@ class server():
                 if(location != None):
                         self.kernel['devel'] = output[location.start():location.end()]
                 else:
-                        self.kernel['firmware'] = None       
-                        
+                        self.kernel['firmware'] = None
+
         def get_repos(self, output):
                 for regex in repos_strings:
                         self.repos.append(re.findall(repos_strings[regex], output, re.I))
 
         def make_report(self):
-                print(BETWEEN_METHODS)
 
-                print('Hostname: {0}'.format(self.hostname))
+                report = open("BeforePatchCheck.log", "a+")
 
-                print('Server release: {0}'.format(self.release))
+                report.write('{0}\n'.format(BETWEEN_METHODS))
+                report.write('{0}\n'.format(datetime.datetime.now()))
+                report.write('Hostname: {0}\n'.format(self.hostname))
+                report.write('Server release: {0}\n'.format(self.release))
 
-                if(int(self.qpk[-4:]) < regular_expressions['qpk'][1]): print('QPK version: {0}, needed version: {1}'.format(self.qpk, regular_expressions['qpk'][1]))
-                else: print('QPK version: {0}, no update needed'.format(self.qpk))
+                if(int(self.qpk[-4:]) < regular_expressions['qpk'][1]): report.write('QPK version: {0}, needed version: {1}\n'.format(self.qpk, regular_expressions['qpk'][1]))
+                else: report.write('QPK version: {0}, no update needed\n'.format(self.qpk))
 
-                if(self.root_space >= 85): print('Root space: {0}%, need more than 15% to patch'.format(abs(self.root_space-100)))
-                else: print('Root space: {0}%'.format(abs(self.root_space-100)))
+                if(self.root_space >= 85): report.write('Root space: {0}%, need more than 15% to patch\n'.format(abs(self.root_space-100)))
+                else: report.write('Root space: {0}%\n'.format(abs(self.root_space-100)))
 
-                if(int(self.kernel['kernel'][-3:]) < regular_expressions['kernel'][1]): print("Kernel: {0}, needed version: {1}".format(self.kernel['kernel'], regular_expressions['kernel'][1]))
-                else: print('Kernel: {0}, no update needed'.format(self.kernel['kernel']))
+                if(int(self.kernel['kernel'][-3:]) < regular_expressions['kernel'][1]): report.write("Kernel: {0}, needed version: {1}\n".format(self.kernel['kernel'], regular_expressions['kernel'][1]))
+                else: report.write('Kernel: {0}, no update needed\n'.format(self.kernel['kernel']))
 
                 if(self.kernel['firmware'] == None):
-                        print('No kernel-firmware detected')
+                        report.write('No kernel-firmware detected\n')
                 else:
-                        if(int(self.kernel['firmware'][-3:]) < regular_expressions['firmware'][1]): print("Kernel-firmware: {0}, needed version: {1}".format(self.kernel['firmware'], regular_expressions['firmware'][1]))
-                        else: print('Kernel-firmware: {0}, no update neeeded'.format(self.kernel['firmware']))
+                        if(int(self.kernel['firmware'][-3:]) < regular_expressions['firmware'][1]): report.write("Kernel-firmware: {0}, needed version: {1}\n".format(self.kernel['firmware'], regular_expressions['firmware'][1]))
+                        else: report.write('Kernel-firmware: {0}, no update neeeded\n'.format(self.kernel['firmware']))
 
                 if(self.kernel['devel'] == None):
-                        print('No kernel-devel detected')
+                        report.write('No kernel-devel detected\n')
                 else:
-                        if(int(self.kernel['devel'][-3:]) < regular_expressions['devel'][1]): print('Kernel-devel: {0}, needed version: {1}'.format(self.kernel['devel'], regular_expressions['devel'][1]))
-                        else: print('Kernel-devel: {0}, no update needed'.format(self.kernel['devel']))
+                        if(int(self.kernel['devel'][-3:]) < regular_expressions['devel'][1]): report.write('Kernel-devel: {0}, needed version: {1}\n'.format(self.kernel['devel'], regular_expressions['devel'][1]))
+                        else: report.write('Kernel-devel: {0}, no update needed\n'.format(self.kernel['devel']))
 
-                print('Repos in our scope (check for comments inside each one):')
+                report.write('Repos in our scope (check for comments inside each one):\n')
                 for x in range(0, len(self.repos)):
                         for y in range(0, len(self.repos[x])):
-                                print('---{0}{1}'.format(commands['repos'][1], self.repos[x][y]))
+                                report.write('---{0}{1}\n'.format(commands['repos'][1], self.repos[x][y]))
 
-                print(BETWEEN_METHODS)          
+                report.write('{0}\n'.format(BETWEEN_METHODS))
+                report.write('It took {0:.4} seconds to run\n'.format(time.time()-self.time_to_run))
+                report.close()
+
+        def print_for_table(self):
+                print('{0:40}  {1:40}'.format(self.hostname, self.qpk))
 
         def start_server_check(self):
-                time_to_run = time.time()
-                self.get_hostname(self.get_command_output(commands['hostname'], None))
-                self.get_release(self.get_command_output(commands['release'], None))
-                self.get_qpk(self.get_command_output(commands['qpk'][0], commands['qpk'][1]))
-                self.get_root_space(self.get_command_output(commands['root_space'][0], commands['root_space'][1]))
-                self.get_kernel(self.get_command_output(commands['kernel'][0], commands['kernel'][1]))
-                self.get_repos(self.get_command_output(commands['repos'], None))
+                self.get_hostname(get_command_output(commands['hostname'], None))
+                self.get_release(get_command_output(commands['release'], None))
+                self.get_qpk(get_command_output(commands['qpk'][0], commands['qpk'][1]))
+                self.get_root_space(get_command_output(commands['root_space'][0], commands['root_space'][1]))
+                self.get_kernel(get_command_output(commands['kernel'][0], commands['kernel'][1]))
+                self.get_repos(get_command_output(commands['repos'], None))
                 self.make_report()
-                print('It took {0:.4} seconds to run'.format(time.time()-time_to_run))
+                self.print_for_table()
 
 test_server = server()
