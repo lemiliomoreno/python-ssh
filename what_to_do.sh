9f62ebb80164bef9c81c18ab68a48577b6131cc7
lemiliomoreno <lemiliomoreno@gmail.com>
2018-11-30 11:26:26 -0600

    Update what_to_do.sh

--- what_to_do.sh
+++ what_to_do.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-# ONYL CHANGE THIS VARIABLE #
-USERNAME="username"
+source .private_files/login.sh
 
 SERVERS=$(cat ~/before_patch_report/servers.txt)
 SCRIPT_PATH="${HOME}/before_patch_report/before_patch_check.py"
@@ -11,12 +10,12 @@ PASSWORD_PATH="${HOME}/before_patch_report/.private_files/passwd.txt"
 
 for server in ${SERVERS}
 do
-        sshpass -f ${PASSWORD_PATH} scp -o ConnectTimeout=10 -o StrictHostKeyChecking=no ${SCRIPT_PATH} ${USERNAME}@${server}:${REMOTE_PATH}
+        sshpass -p ${PASSWORD} scp -o ConnectTimeout=10 -o StrictHostKeyChecking=no ${SCRIPT_PATH} ${USERNAME}@${server}:${REMOTE_PATH}
         if [ $? -ne 0 ]
         then
                 echo "SCP Error in ${server}"
         fi
-        sshpass -f ${PASSWORD_PATH} ssh -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no ${USERNAME}@${server} < ${COMMAND_PATH}
+        sshpass -p ${PASSWORD} ssh -T -o ConnectTimeout=10 -o StrictHostKeyChecking=no ${USERNAME}@${server} < ${COMMAND_PATH}
         if [ $? -ne 0 ]
         then
                 echo "SSH Error in ${server}"
